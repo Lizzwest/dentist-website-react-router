738ea3e323f8f6244d496c193c024e374e86f3cd
Lizz <lizwesterband@gmail.com>
2020-08-28 11:15:57 -0700

    [app] Add links

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ function App() {
   return (
     <Router>
 <div className="App">
+  <nav>
+    <Link to="/">Go Back To Home</Link>{"  "}
+    <Link to="/procedures">See our procedures</Link>{"  "}
+    <Link to="/contact">Contact us</Link><br />
+  </nav>
       <Route exact path="/" component={Home}/>
       <Route path="/procedures" component={Procedures}/>
       <Route path="/contact" component={Contact}/>
