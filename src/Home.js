fc7cd8ff52a18655bac71c08abf399c1f229594d
Lizz <lizwesterband@gmail.com>
2020-08-28 10:52:31 -0700

    [Home] Add home component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Harmony Dentistry</h1>
+      <p> 4 out of 5 dentists recommend us. </p>
+            </div>
+        )
+    }
+}
+
+export default Home;
